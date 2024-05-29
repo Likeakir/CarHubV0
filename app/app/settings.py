10d93ae9958cd20a130f37f6bc0a067c14d12fb3
alfredo <alfredo.parrenorusso@gmail.com>
2024-05-29 15:36:56 +0200

    changes

--- app/app/settings.py
+++ app/app/settings.py
@@ -16,6 +16,7 @@ from pathlib import Path
 import environ
 import google.auth
 from google.cloud import secretmanager
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
